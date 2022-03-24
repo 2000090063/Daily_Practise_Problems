5a8a557d0433ebc720b484481bacb239132810a2
Karthik Reddy Veluru <2000090063@kluniversity.in>
2022-03-24 21:17:55 +0530

    Create Largest-number-formed-from-array.cpp

---
+++ Largest-number-formed-from-array.cpp
@@ -0,0 +1,76 @@
+/*Given a list of non negative integers, arrange them in such a manner that they form the largest number possible.The result is going to be very large, hence return the result in the form of a string.
+
+
+Example 1:
+
+Input: 
+N = 5
+Arr[] = {3, 30, 34, 5, 9}
+Output: 9534330
+Explanation: Given numbers are {3, 30, 34,
+5, 9}, the arrangement 9534330 gives the
+largest value.
+Example 2:
+
+Input: 
+N = 4
+Arr[] = {54, 546, 548, 60}
+Output: 6054854654
+Explanation: Given numbers are {54, 546,
+548, 60}, the arrangement 6054854654 
+gives the largest value.
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function printLargest() which takes the array of strings arr[] as parameter and returns a string denoting the answer.
+
+Expected Time Complexity: O(NlogN)
+Expected Auxiliary Space: O(1)
+
+*/
+
+//solution
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	// The main function that returns the arrangement with the largest value as
+	// string.
+	// The function accepts a vector of strings
+	static int mc(string x,string y)
+	{
+	    string xy=x.append(y);
+	    string yx=y.append(x);
+	    return (xy>yx);
+	}
+	string printLargest(vector<string> &arr) {
+	    // code here
+	    string s="";
+	    sort(arr.begin(),arr.end(),mc);
+	    for(auto i:arr)
+	      s+=i;
+	      return s;
+	}
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<string> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.printLargest(arr);
+        cout << ans << "\n";
+    }
+    return 0;
+}  // } Driver Code Ends
