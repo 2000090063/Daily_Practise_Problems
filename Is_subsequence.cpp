4e60f4ffa092668de06e194ba0969d7599d0b6f4
Karthik Reddy Veluru <2000090063@kluniversity.in>
2022-07-19 17:44:26 +0530

    Create Is_subsequence.cpp

---
+++ Is_subsequence.cpp
@@ -0,0 +1,23 @@
+
+
+
+
+/********************************************************USING TWO POINTER METHOD****************************************************************/
+bool is_subsequence(string s,string t)
+{
+int n = s.length(),m=t.length();
+        int j = 0; 
+    // For index of s (or subsequence
+ 
+    // Traverse s and t, and
+    // compare current character
+    // of s with first unmatched char
+    // of t, if matched
+    // then move ahead in s
+    for (int i = 0; i < m and j < n; i++)
+        if (s[j] == t[i])
+            j++;
+ 
+    // If all characters of s were found in t
+    return (j == n);
+    }
